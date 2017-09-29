c676da908d2a8c1a449b1d325edc10b60e3aa0e7
jonathan Morton <jmorton4193@gmail.com>
2017-09-29 00:33:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,10 @@ console.log('HELLO'.toLowerCase())
 }
 
 function sayHiToGrandma (string) {
-if (string==='hello') {console.log('I can\'t hear you!')}
-else if (string==='HELLO') {console.log('YES INDEED!')}
-else (string==='I love you, Grandma.') {console.log('I love you, too.')}
+console.log($('string'))
+
+  return string
 }
+sayHiToGrandma('hello')
+sayHiToGrandma('HELLO')
+sayHiToGrandma('I love you, Grandma.')
